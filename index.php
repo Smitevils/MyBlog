08c6b7ee7210384c57ea370cf2d2e0182abddef5
Alexey <SmiteVils@yandex.ru>
2015-01-31 11:23:53 +0300

    Добавил admin.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="UTF-8">
 	<title>SmiteVils Blog</title>
-	<link rel="stylesheet" href="normalize.css">
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="styles/normalize.css">
+	<link rel="stylesheet" href="styles/style.css">
 	<script src="js/jquery-2.1.1.js"></script>
 </head>
 <body>
