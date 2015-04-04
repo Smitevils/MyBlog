0ef699c5fd463e2c11607f8d224a64425b8d6dae
Alexey <SmiteVils@yandex.ru>
2015-04-04 17:24:51 +0300

    Добавил фавикон

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 <html lang="en">
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->
+	<link href="img/favicon.ico" rel="shortcut icon" type="image/x-icon" />
 	<meta charset="UTF-8">
 	<title>SmiteVils Blog</title>
 	<!-- Внешние стили -->
